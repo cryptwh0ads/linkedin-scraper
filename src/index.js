16e7687ec6dc125c1b777231c02f6c0e906b4662
Vinicius Cruz <wh0adscrypt@gmail.com>
2020-04-07 11:54:11 -0300

    feat: Created basic scraper

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const scrapedin = require("scrapedin");
+
+const options = {
+  email: "your@email.com",
+  password: "yourPass",
+  hasToGetContactInfo: true,
+};
+
+const getProfile = () => {
+  scrapedin(options)
+    .then((profileScraper) =>
+      profileScraper("https://www.linkedin.com/in/some-profile"),
+    )
+    .then((profile) => console.log(profile));
+};
+
+getProfile();
+
+module.exports = getProfile;
