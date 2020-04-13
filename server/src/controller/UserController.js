6af5673460ef6ecc11bb1428dfead72d4e8fa66b
Vinicius Cruz <wh0adscrypt@gmail.com>
2020-04-13 09:57:39 -0300

    feat: Add remove route

--- server/src/controller/UserController.js
+++ server/src/controller/UserController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+// List all users
 const index = async (req, res) => {
   const users = await User.findAll();
 
@@ -11,6 +12,7 @@ const index = async (req, res) => {
   }
 };
 
+// Create user
 const store = (req, res) => {
   let { name, last_name, email, passwd, role } = req.body;
 
@@ -47,7 +49,34 @@ const store = (req, res) => {
   }
 };
 
+const remove = async (req, res) => {
+  const { id } = req.params;
+  try {
+    User.findOne({
+      where: {
+        id,
+      },
+    }).then(async (user) => {
+      if (user) {
+        const deleted = await User.destroy({
+          where: {
+            id,
+          },
+        });
+        if (deleted) {
+          return res.status(200).send({ data: `User has been deleted!` });
+        }
+      } else {
+        return res.status(404).send({ error: `User couldn't found` });
+      }
+    });
+  } catch (err) {
+    return res.status(400).send({ error: err });
+  }
+};
+
 module.exports = {
   store,
   index,
+  remove,
 };
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -3,5 +3,6 @@ const UserController = require("../controller/UserController");
 
 router.get("/", UserController.index);
 router.post("/", UserController.store);
+router.delete("/:id", UserController.remove);
 
 module.exports = router;
